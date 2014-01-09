ec4870219334060c25ccb7d4c2f3e82d13569949
Espen Riskedal <espen@cutehacks.com>
2014-01-09 16:58:43 +0100

    basic splash and then main after 500 ms

---
+++ src/com/cutehacks/examplesplashscreen/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.cutehacks.examplesplashscreen;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, 500);
+    }
+}
