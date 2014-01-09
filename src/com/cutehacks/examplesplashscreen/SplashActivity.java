6386d8fdf5d0f8201f57fe6d42f9fbd38bdfe0ad
Espen Riskedal <espen@cutehacks.com>
2014-01-09 18:29:18 +0100

    added comment

--- src/com/cutehacks/examplesplashscreen/SplashActivity.java
+++ src/com/cutehacks/examplesplashscreen/SplashActivity.java
@@ -10,7 +10,7 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_splash); // not needed - at the moment it is completely empty
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
